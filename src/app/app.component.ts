c17264c972ad1528b379c6f1c4783fe30eba990b
Joseph B <mcjoseph789@gmail.com>
2021-10-16 10:37:33 -0500

    Cronometro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'proyect005';
+  mensaje='';
+  actualizar(t:Event){
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CronometroComponent } from './cronometro/cronometro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CronometroComponent
   ],
   imports: [
     BrowserModule,
